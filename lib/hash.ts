ce3bf9e58726409bd37f9558393a1504f1a66fca
Victoria Rodriguez <vrodriguezfe@icloud.com>
2024-01-04 21:47:33 -0300

    docs(hash): Describe the function `md5`

--- lib/hash.ts
+++ lib/hash.ts
@@ -1,5 +1,11 @@
 import { createHash } from "node:crypto";
 
+/**
+ * Calculates the MD5 hash of the given string value.
+ *
+ * @param value - The string to hash.
+ * @returns The MD5 hash of the input string in hexadecimal format.
+ */
 export function md5(value: string) {
   return createHash("md5").update(value, "utf-8").digest("hex");
 }
