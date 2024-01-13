ccb9a205a0c7209a7e62b70b57948279adfb4e0e
Victoria Rodriguez <vrodriguezfe@icloud.com>
2024-01-13 15:06:20 -0300

    chore(config): Use Prettier instead of Deno for formatting

---
+++ prettier.config.js
@@ -0,0 +1,65 @@
+/**
+ * @type {PrettierConfig & Partial<PJPOptions>}
+ */
+export default {
+  trailingComma: "all",
+  tabWidth: 2,
+  semi: true,
+  singleQuote: false,
+  printWidth: 90,
+  proseWrap: "always",
+  quoteProps: "consistent",
+  useTabs: false,
+  endOfLine: "lf",
+  bracketSameLine: true,
+  plugins: ["@homer0/prettier-plugin-jsdoc"],
+  jsdocEnsureDescriptionsAreSentences: true,
+  jsdocPrintWidth: 90,
+  jsdocTagsOrder: ["example", "template", "param", "returns", "namespace", "typedef"],
+  jsdocFormatExamples: false,
+  jsdocUseTypeScriptTypesCasing: false,
+  parser: "typescript",
+  jsdocMinSpacesBetweenNameAndDescription: 1,
+};
+
+/**
+ * @typedef {import("prettier").Config} PrettierConfig
+ */
+
+/**
+ * Options for configuring Prettier JSDoc plugin behavior.
+ *
+ * @typedef {{
+ *   jsdocAllowDescriptionTag: boolean;
+ *   jsdocUseDescriptionTag: boolean;
+ *   jsdocFormatExamples: boolean;
+ *   jsdocLinesBetweenExampleTagAndCode: number;
+ *   jsdocIndentFormattedExamples: boolean;
+ *   jsdocIndentUnformattedExamples: boolean;
+ *   jsdocAllowAccessTag: boolean;
+ *   jsdocEnforceAccessTag: boolean;
+ *   jsdocUseTypeScriptTypesCasing: boolean;
+ *   jsdocFormatComplexTypesWithPrettier: boolean;
+ *   jsdocUseShortArrays: boolean;
+ *   jsdocFormatDotForArraysAndObjects: boolean;
+ *   jsdocUseDotForArraysAndObjects: boolean;
+ *   jsdocFormatStringLiterals: boolean;
+ *   jsdocUseSingleQuotesForStringLiterals: boolean;
+ *   jsdocSpacesBetweenStringLiterals: number;
+ *   jsdocUseColumns: boolean;
+ *   jsdocGroupColumnsByTag: boolean;
+ *   jsdocConsistentColumns: boolean;
+ *   jsdocDescriptionColumnMinLength: number;
+ *   jsdocMinSpacesBetweenTagAndType: number;
+ *   jsdocMinSpacesBetweenTypeAndName: number;
+ *   jsdocMinSpacesBetweenNameAndDescription: number;
+ *   jsdocLinesBetweenDescriptionAndTags: number;
+ *   jsdocEnsureDescriptionsAreSentences: boolean;
+ *   jsdocAllowDescriptionOnNewLinesForTags: string[];
+ *   jsdocIgnoreNewLineDescriptionsForConsistentColumns: boolean;
+ *   jsdocUseInlineCommentForASingleTagBlock: boolean;
+ *   jsdocPrintWidth: number;
+ *   jsdocPluginEnabled: boolean;
+ *   jsdocPluginExtended: boolean;
+ * }} PJPOptions
+ */
