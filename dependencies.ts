74a7f06a84aa5ecc9aea77f9083c24e6c20f71ea
Victoria Rodriguez <vrodriguezfe@icloud.com>
2024-01-13 15:05:18 -0300

    build(deps): Export from a single file

---
+++ dependencies.ts
@@ -0,0 +1,7 @@
+export { eld } from "eld";
+export { default as Kuroshiro } from "kuroshiro";
+export { default as KuromojiAnalyzer } from "kuroshiro-analyzer-kuromoji";
+export { z } from "zod";
+export * as toolkit from "toolkit";
+export { default as transliterateRussian } from "translit-rus-eng";
+export { default as natural } from "natural";
