a49260de2d31d2ad42bd071a63c1f4b8bfab1471
Victoria Rodriguez <vrodriguezfe@icloud.com>
2024-01-17 14:37:23 -0300

    chore(deps): Use dev dependencies identifiers directly in file

--- dev-dependencies.ts
+++ dev-dependencies.ts
@@ -1,7 +1,7 @@
 export * from "@dependencies";
-export { MockFetch } from "mock-fetch";
+export { MockFetch } from "https://deno.land/x/deno_mock_fetch@1.0.1/mod.ts";
 export { STATUS_TEXT, STATUS_CODE } from "std/http/status.ts";
 export { describe, it } from "std/testing/bdd.ts";
-// @deno-types="@types/chai"
-export { expect } from "chai";
-export { faker } from "faker";
+// @deno-types="npm:@types/chai@4"
+export { expect } from "npm:chai@4.4.1";
+export { faker } from "npm:@faker-js/faker";
