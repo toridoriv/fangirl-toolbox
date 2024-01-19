edb4b652d33aaa374bbf47a774a96abdf6ee029d
Victoria Rodriguez <vrodriguezfe@icloud.com>
2024-01-19 15:21:22 -0300

    chore(base): Update main module exports

--- base/mod.ts
+++ base/mod.ts
@@ -1,5 +1,6 @@
 export * from "./actions.ts";
 export * from "./clients.ts";
-export * from "./global.ts";
 export * from "./models.ts";
 export * from "./utils.ts";
+export * from "./repositories.ts";
+export * from "./services.ts";
