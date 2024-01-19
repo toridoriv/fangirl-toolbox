9bf0aee2fbd624a8b65d8899222b31e475cd64be
Victoria Rodriguez <vrodriguezfe@icloud.com>
2024-01-18 22:00:40 -0300

    chore(config): Do not format development dependencies

--- dev-dependencies.ts
+++ dev-dependencies.ts
@@ -2,6 +2,14 @@ export * from "@dependencies";
 export { MockFetch } from "https://deno.land/x/deno_mock_fetch@1.0.1/mod.ts";
 export { STATUS_TEXT, STATUS_CODE } from "std/http/status.ts";
 export { describe, it } from "std/testing/bdd.ts";
-// @deno-types="npm:@types/chai@4"
-export { expect } from "npm:chai@4.4.1";
 export { faker } from "npm:@faker-js/faker";
+
+// @deno-types="npm:@types/chai@4"
+import chai from "npm:chai@4.4.1";
+
+// @deno-types="npm:@types/chai-as-promised"
+import chaiAsPromised from "npm:chai-as-promised";
+
+chai.use(chaiAsPromised);
+
+export const { expect } = chai;
