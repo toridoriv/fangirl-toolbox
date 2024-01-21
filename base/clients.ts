017b4034c6d02ca33e7fa4ec9be50870df14c6f2
Victoria Rodriguez <vrodriguezfe@icloud.com>
2024-01-21 15:46:49 -0300

    refactor(base)!: The `WebScraper` client now returns `CheerioAPI` and the source html code

--- base/clients.ts
+++ base/clients.ts
@@ -544,6 +544,24 @@ export class OpenAiClient extends HttpClient<typeof OpenAiClient> {
  */
 export class WebScraper extends HttpClient<typeof WebScraper> {
   /**
+   * Converts HTML string to a Cheerio static object.
+   * This allows for jQuery-like manipulation and querying of the HTML content.
+   * If a URL is provided, it sets the base URL for the document, which can be useful for
+   * resolving relative paths.
+   *
+   * @param html - The HTML content as a string.
+   * @param url  - Optional base URL to be used for resolving relative paths within the
+   *             HTML content.
+   * @returns A Cheerio static object representing the loaded HTML content.
+   */
+  public static toCheerio(html: string, url?: string | URL) {
+    return cheerioLoad(html, {
+      baseURI: url,
+      sourceCodeLocationInfo: true,
+    });
+  }
+
+  /**
    * Default configuration for web scraping requests.
    */
   static defaults: HttpClient.Config = {
@@ -553,15 +571,22 @@ export class WebScraper extends HttpClient<typeof WebScraper> {
   };
 
   /**
-   * Scrapes the HTML from a web page.
+   * Performs web scraping based on the provided configuration.
    *
-   * @param config - The HTTP client configuration.
-   * @returns A Cheerio instance containing the loaded HTML.
+   * This method sends an HTTP request using the given configuration and then processes
+   * the response to extract HTML content. It utilizes the `WebScraper.toCheerio` method
+   * to convert the HTML source into a Cheerio object, allowing for jQuery-like
+   * manipulation.
+   * The method returns an object containing the Cheerio wrapper and the raw HTML source.
+   *
+   * @param config - The HTTP client configuration for the request.
+   * @returns An object with a Cheerio wrapper and the raw HTML source of the scraped
+   *          page.
    */
   public async scrape(config: HttpClient.Config) {
     const response = await this.request<string>(config);
-    const html = response.data;
+    const source = response.data;
 
-    return cheerioLoad(html);
+    return { $: WebScraper.toCheerio(source, response.request.url), source };
   }
 }
