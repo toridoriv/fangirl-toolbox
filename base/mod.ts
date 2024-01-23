afa91ea932b533eaac2dca4044f81c289abcb282
Victoria Rodriguez <vrodriguezfe@icloud.com>
2024-01-22 21:24:49 -0300

    style(base): Order exports alphabetically

--- base/mod.ts
+++ base/mod.ts
@@ -1,6 +1,6 @@
 export * from "./actions.ts";
 export * from "./clients.ts";
 export * from "./models.ts";
-export * from "./utils.ts";
 export * from "./repositories.ts";
 export * from "./services.ts";
+export * from "./utils.ts";
