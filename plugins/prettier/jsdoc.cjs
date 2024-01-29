b7deb443abd14676650780b5a7b5a73759e1fdc7
Victoria Rodriguez <vrodriguezfe@icloud.com>
2024-01-29 18:25:00 -0300

    chore(config): Remove dead code

--- plugins/prettier/jsdoc.cjs
+++ plugins/prettier/jsdoc.cjs
@@ -2,10 +2,6 @@ const { get, override } = require("@homer0/prettier-plugin-jsdoc/src/fns/app");
 const { loadFns } = require("@homer0/prettier-plugin-jsdoc/src/loader");
 const { getPlugin } = require("@homer0/prettier-plugin-jsdoc/src/fns/getPlugin");
 const { splitText } = require("@homer0/prettier-plugin-jsdoc/src/fns/splitText");
-// const { renderTagsInlines } = require("@homer0/prettier-plugin-jsdoc/src/fns/render");
-// const utils = require("@homer0/prettier-plugin-jsdoc/src/fns/utils");
-// const { sortTags } = require("@homer0/prettier-plugin-jsdoc/src/fns/sortTags");
-// const formatTags = require("@homer0/prettier-plugin-jsdoc/src/fns/formatTags");
 
 loadFns();
 
@@ -17,52 +13,6 @@ function customSplitText(text, length) {
   return splitText(text, length);
 }
 
-// function customRenderTagsInlines(width, options, tags) {
-//   const typedefs = tags.filter(isTypeDef);
-//   const rest = tags.filter(isNotTypeDef);
-
-//   return [...typedefs.map(renderTag), ...renderTagsInlines(width, options, rest)];
-// }
-
-// function customSortTags(tags, options) {
-//   return function (tags) {
-//     console.log(tags[0]);
-//     return tags;
-//   };
-// }
-
-// function customGetIndexOrFallback(list, fallback, item) {
-//   console.log({ list, item });
-//   if (item === "see") {
-//     return () => 0;
-//   }
-
-//   return () => utils.getIndexOrFallback(list.reverse(), 100, item || "other");
-// }
-
-// function customFormatTags(tags, options) {
-//   return (tags) => {
-//     console.log("customFormatTags", { result: formatTags.formatTags(tags, options) });
-//     return formatTags.formatTags(tags, options);
-//   };
-// }
-
-// function isTypeDef(tag) {
-//   return tag.tag === "typedef" && tag.source.length <= 2;
-// }
-
-// function isNotTypeDef(tag) {
-//   return !isTypeDef(tag);
-// }
-
-// function renderTag(tag) {
-//   return `@${tag.tag} {${tag.type}} ${tag.name}`;
-// }
-
 override(splitText, customSplitText);
-// override(renderTagsInlines, customRenderTagsInlines);
-// // override(sortTags, customSortTags);
-// override(utils.getIndexOrFallback, customGetIndexOrFallback);
-// // override(formatTags.formatTags, customFormatTags);
 
 module.exports = get(getPlugin)();
