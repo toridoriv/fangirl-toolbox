f2c87084c5d203437fe03a93b6d9fd49bee660ba
Victoria Rodriguez <vrodriguezfe@icloud.com>
2024-02-03 21:24:26 -0300

    chore(config): Setup tests

---
+++ jest.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import("jest").Config} */
+const config = {
+  verbose: true,
+  transform: {},
+  collectCoverage: true,
+  collectCoverageFrom: ["./lib/**/*.js"],
+};
+
+export default config;
